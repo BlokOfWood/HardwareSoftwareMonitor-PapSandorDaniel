8f87368aa9e021ab5afbf20a998f16bdb7cde433
StrikerElite <strikerelite@outlook.hu>
2021-04-14 18:12:02 +0200

    project létrehozás

---
+++ HardwareSoftwareMonitor/HardwareSoftwareMonitor/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Windows;
+
+namespace HardwareSoftwareMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
