b0f6898afd9269b8faabe5bb459ba42675b4a2d0
StrikerElite <strikerelite@outlook.hu>
2021-04-22 17:51:59 +0200

    hardver elem kiírás

--- HardwareSoftwareMonitor/HardwareSoftwareMonitor/MainWindow.xaml.cs
+++ HardwareSoftwareMonitor/HardwareSoftwareMonitor/MainWindow.xaml.cs
@@ -13,7 +13,27 @@ namespace HardwareSoftwareMonitor
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            /*Hardverelemek*/
+            MotherboardManufacturer.Content = GetComponent("Win32_BaseBoard", "Manufacturer");
+            MotherboardProduct.Content = GetComponent("Win32_BaseBoard", "Product");
+            Processor.Content = GetComponent("Win32_Processor", "Name");
+            Videocard.Content = GetComponent("Win32_VideoController", "Name");
+            BIOSManufacturer.Content = GetComponent("Win32_BIOS", "Manufacturer");
+            BIOS_Name.Content = GetComponent("Win32_BIOS", "Name");
+        }
+        static string GetComponent(string hardwareClass, string syntax)
+        {
+            string returnValue = "";
+
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM " + hardwareClass);
+            foreach (ManagementObject mj in mos.Get())
+            {
+                if (Convert.ToString(mj[syntax]) != "")
+                    returnValue += Convert.ToString(mj[syntax]);
+            }
+
+            return returnValue;
         }
     }
 }
